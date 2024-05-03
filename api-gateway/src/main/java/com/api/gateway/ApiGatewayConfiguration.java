aac8014ec6319efcda8d0da2d9746d979191155f
Sachin Prasad <42201087+sachinprasad3@users.noreply.github.com>
2024-05-03 11:55:14 +0530

    first-microservice

---
+++ api-gateway/src/main/java/com/api/gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,36 @@
+package com.api.gateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p->p.path("/get")
+					.filters(f->f.addRequestHeader("MyHeader", "MyURI")
+							.addRequestParameter("param", "MyVAlue"))
+					.uri("http://httpbin.org:80"))
+				
+				.route(p->p.path("/currency-exchange/**") //hitting UIR
+						.uri("lb://currency-exchange-service")) //Service to be Called in EUREKA
+				.route(p->p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion-service"))
+				.route(p->p.path("/currency-conversion-feign/**")
+						.uri("lb://currency-conversion-service"))
+				.route(p->p.path("/currency-conversion-new/**")
+						.filters(f->f.rewritePath("/currency-conversion-new/(?<segment>.*)", "/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion-service"))
+				.build();
+		
+	}
+}
