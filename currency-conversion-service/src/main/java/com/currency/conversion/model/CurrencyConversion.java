e97a74c32953b7f2fb6f8d1261d0cb47a079b7a3
Sachin Prasad <42201087+sachinprasad3@users.noreply.github.com>
2024-05-03 11:52:27 +0530

    first-microservice

---
+++ currency-conversion-service/src/main/java/com/currency/conversion/model/CurrencyConversion.java
@@ -0,0 +1,21 @@
+package com.currency.conversion.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+	
+	private long id;
+	private String from;
+	private String to;
+	private BigDecimal quantity;
+	private BigDecimal conversionMultiple;
+	private BigDecimal totalCalculatedAmount;
+	private String environment;
+}
