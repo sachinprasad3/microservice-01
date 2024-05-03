8d4d1b9fbd0ce959e3c552ee0a91754da127f5ce
Sachin Prasad <42201087+sachinprasad3@users.noreply.github.com>
2024-05-03 11:53:04 +0530

    first-microservice

---
+++ config-server/src/test/java/com/cloud/server/Limitservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Limitservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
